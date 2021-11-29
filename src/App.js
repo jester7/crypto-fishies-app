66e1da544be005580d9bc09a2cd0d29b8ac9780e
Jovan Jester <jovan@jester.cafe>
2021-11-29 00:21:48 -0600

    updated app title and meta tags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'jovanjester';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -11,8 +11,8 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">CryptoFishies Ocean Battle</p>
+          <p className="sub-text">Team up with other CryptoFishies to defeat large predators!</p>
           <div className="connect-wallet-container">
             <img
               src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
