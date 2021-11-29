2f5049974c887631a27c706e24a8babd59266d82
Jovan Jester <jovan@jester.cafe>
2021-11-29 00:51:23 -0600

    added character selection and refactored rendering

--- src/App.js
+++ src/App.js
@@ -1,43 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SelectCharacter from "./Components/SelectCharacter";
+import twitterLogo from "./assets/twitter-logo.svg";
 
 // Constants
-const TWITTER_HANDLE = 'jovanjester';
+const TWITTER_HANDLE = "jovanjester";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
   const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
 
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
 
       if (!ethereum) {
-        console.log('Make sure you have MetaMask!');
+        console.log("Make sure you have MetaMask!");
         return;
       } else {
-        console.log('We have the ethereum object', ethereum);
+        console.log("We have the ethereum object", ethereum);
 
         /*
          * Check if we're authorized to access the user's wallet
          */
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        const accounts = await ethereum.request({ method: "eth_accounts" });
 
         /*
          * User can have multiple authorized accounts, we grab the first one if its there!
          */
         if (accounts.length !== 0) {
           const account = accounts[0];
-          console.log('Found an authorized account:', account);
+          console.log("Found an authorized account:", account);
           setCurrentAccount(account);
         } else {
-          console.log('No authorized account found');
+          console.log("No authorized account found");
         }
       }
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  // Render Methods
+  const renderContent = () => {
+    /*
+     * Scenario #1
+     */
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://media2.giphy.com/media/CE2JyzhfSycrm/giphy.gif?cid=ecf05e47babc8mk30e83mscbdx4fpq93btal20omckont3ex&rid=giphy.gif&ct=g"
+            alt="Finding Nemo Gif"
+          />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+      /*
+       * Scenario #2
+       */
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
     }
   };
 
@@ -46,7 +75,7 @@ const App = () => {
       const { ethereum } = window;
 
       if (!ethereum) {
-        alert('Get MetaMask!');
+        alert("Get MetaMask!");
         return;
       }
 
@@ -54,19 +83,18 @@ const App = () => {
        * Fancy method to request access to account.
        */
       const accounts = await ethereum.request({
-        method: 'eth_requestAccounts',
+        method: "eth_requestAccounts",
       });
 
       /*
        * Boom! This should print out public address once we authorize Metamask.
        */
-      console.log('Connected', accounts[0]);
+      console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error);
     }
   };
-
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -77,20 +105,11 @@ const App = () => {
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">CryptoFishies Ocean Battle</p>
-          <p className="sub-text">Team up with other CryptoFishies to defeat large predators!</p>
+          <p className="sub-text">
+            Team up with other CryptoFishies to defeat large predators!
+          </p>
         </div>
-        <div className="connect-wallet-container">
-          <img
-            src="https://media2.giphy.com/media/CE2JyzhfSycrm/giphy.gif?cid=ecf05e47babc8mk30e83mscbdx4fpq93btal20omckont3ex&rid=giphy.gif&ct=g"
-            alt="Finding Nemo Gif"
-          />
-          <button
-              className="cta-button connect-wallet-button"
-              onClick={connectWalletAction}
-            >
-              Connect Wallet To Get Started
-            </button>
-        </div>
+        {renderContent()}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
